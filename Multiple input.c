ec0f9784ff23f5956100fe12e0006671182f5e07
jaibhaskar <70481235+jaibhaskar@users.noreply.github.com>
2020-08-30 22:00:28 +0530

    Add files via upload

---
+++ Multiple input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("Enter the  three values :");
+    scanf("%d%d%d",&a,&b,&c);
+    printf("sum of %d+%d+%d is %d",a,b,c,a+b+c);
+    return 0;
+}
